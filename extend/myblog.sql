b378e0de83b5370767d2ee36a45366f4ab5de446
wolf-leo <wolf-leo@users.noreply.github.com>
2018-04-26 17:13:27 +0800

    sql

---
+++ extend/myblog.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `article` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `desc` varchar(250) NOT NULL DEFAULT '',
+  `type` tinyint(3) unsigned NOT NULL,
+  `img` varchar(150) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `c_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `index_title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
