4f51d63bf6c3063138e087a9d0fd8b16d6e71064
wolf-leo <37436228+wolf-leo@users.noreply.github.com>
2018-06-12 00:01:37 +0800

    新增列表页轮播图展示

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -21,6 +21,7 @@ class Index extends BlogBaseController {
         $where[] = ['status', '=', 1];
         if ($type) {
             $where[] = ['type', '=', $type];
+            $map[] = ['type', '=', $type];
         }
         $list = $mod->getList($where, $page, $pageSize);
         if (empty($list)) {
@@ -30,9 +31,17 @@ class Index extends BlogBaseController {
         $pageparam = $mod->_pageparam();
         $Page = new \think\paginator\driver\Bootstrap($list, $pageSize, $page, $count, FALSE, $pageparam);
         $show = $Page->render();
+
+        //      顶部轮播图 start
+        $map[] = ['status', '=', 1];
+        $map[] = ['img', '<>', ''];
+        $tops = $mod->getField($map, 'id,title,img', 'id desc', 5);  //获取存在文章缩略图片的前5条数据
+        //      顶部轮播图 end
+
         $this->assign('page', $show);
         $this->assign('list', $list);
         $this->assign('type', $type);
+        $this->assign('tops', $tops);
         return $this->blogTpl();
     }
 
